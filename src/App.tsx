f86a75d978aba9afd789db8fbd5d405e416ad05b
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-06-26 22:16:49 +0600

    initialized with shadcn/ui

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-red-500">This is a basic todo app</h1>
+      <Button>Test</Button>
+    </div>
+  )
+}
+
+export default App
