746cde5b1a456f15815d4b3364ff518cd1b9e832
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-06-29 00:29:51 +0600

    task filter added

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,26 +1,34 @@
 import { AddTaskModal } from "@/components/module/tasks/AddTaskModal";
 import TaskCard from "@/components/module/tasks/TaskCard";
-import { selectTasks } from "@/redux/features/task/taskSlice";
-import { useAppSelector } from "@/redux/hook";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { selectTasks, updateFilter } from "@/redux/features/task/taskSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hook";
 
 const Tasks = () => {
-    const tasks = useAppSelector(selectTasks);
-    console.log(tasks);
-    return (
-        <div className="mx-auto max-w-7xl px-5 mt-20">
-            <div className="flex justify-between items-center">
-                <h1>Tasks</h1>
-                <AddTaskModal></AddTaskModal>
-            </div>
-            <div className="space-y-5 mt-5">
-                {
-                    tasks.map((task) => (
-                        <TaskCard task={task} key={task.id}></TaskCard>
-                    ))
-                }
-            </div>
-        </div>
-    );
+  const tasks = useAppSelector(selectTasks);
+  const dispatch = useAppDispatch();
+  console.log(tasks);
+  return (
+    <div className="mx-auto max-w-7xl px-5 mt-20">
+      <div className="flex justify-end items-center gap-6">
+        <h1 className="mr-auto">Tasks</h1>
+        <Tabs defaultValue="all">
+          <TabsList>
+            <TabsTrigger onClick={() => dispatch(updateFilter("all"))} value="all">All</TabsTrigger>
+            <TabsTrigger onClick={() => dispatch(updateFilter("low"))} value="low">Low</TabsTrigger>
+            <TabsTrigger onClick={() => dispatch(updateFilter("medium"))} value="medium">Medium</TabsTrigger>
+            <TabsTrigger onClick={() => dispatch(updateFilter("high"))} value="high">High</TabsTrigger>
+          </TabsList>
+        </Tabs>
+        <AddTaskModal></AddTaskModal>
+      </div>
+      <div className="space-y-5 mt-5">
+        {tasks.map((task) => (
+          <TaskCard task={task} key={task.id}></TaskCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Tasks;
